9bd4c3f2e5115c7da54ac41e0e765bef2398d017
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2008-10-28 16:20:24 +0100

    add evaluator for the zipper

--- Machines/PolishVM.hs
+++ Machines/PolishVM.hs
@@ -50,6 +50,8 @@ evalL (App f) = case evalL f of
 evalL x = x
 
 
+-- The zipper for efficient evaluation:
+
 -- Arbitrary expressions in Reverse Polish notation.
 -- This can also be seen as an automaton that transforms a stack.
 -- RPolish is indexed by the types in the stack consumed by the automaton.
@@ -58,6 +60,13 @@ data RPolish input output where
   RApp :: RPolish (b :< rest) output -> RPolish ((a -> b) :< a :< rest) output 
   RStop :: RPolish rest rest
 
+-- Evaluate the output of an RP automaton, given an input stack
+evalRP :: RPolish input output -> input -> output
+evalRP RStop acc = acc 
+evalRP (RVal v r) acc = evalRP r (v :< acc)
+evalRP (RApp r) (f :< a :< acc) = evalRP r (f a :< acc)
+
+
 -- execute the automaton as far as possible
 simplify :: RPolish s output -> RPolish s output
 simplify (RVal a (RVal f (RApp r))) = simplify (RVal (f a) r)
@@ -65,11 +74,10 @@ simplify x = x
 
 -- Gluing a Polish expression and an RP automaton.
 -- This can also be seen as a zipper of Polish expressions.
--- Zip could be indexed by the types produced in the final stack. (see the Agda file)
 data Zip output where
    Zip :: RPolish stack output -> Steps stack -> Zip output
    -- note that the Stack produced by the Polish expression matches
-   -- the stack consumed by the automaton.
+   -- the stack consumed by the RP automaton.
 
 -- Move the zipper to the right, if possible.  The type gives evidence
 -- that this function does not change the (type of) output produced.
@@ -78,4 +86,7 @@ right (Zip l (Val a r)) = Zip (RVal a l) r
 right (Zip l (App r)) = Zip (RApp l) r
 right (Zip l s) = (Zip l s)
 
+
+evalZ :: Zip output -> output
+evalZ (Zip rp p) = evalRP rp (evalR p)
 
