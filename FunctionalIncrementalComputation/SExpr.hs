c616d980d8a173af0ac99b67de5d03a09864c10e
Jean-Philippe Bernardy <bernardy@dhcp-197-149.nomad.chalmers.se>
2008-11-06 13:10:08 +0100

    tiny fixes after chat with Koen

--- FunctionalIncrementalComputation/SExpr.hs
+++ FunctionalIncrementalComputation/SExpr.hs
@@ -6,9 +6,10 @@ import Data.Tree
 data SExpr = Cons SExpr SExpr | Leaf Char | Tip
    deriving Show
 
+eof = case_ (pure ()) (error "expected eof")
 
 readSExpr :: P Char SExpr
-readSExpr = case_ (pure Tip) $
+readSExpr = case_ (error "unexpected end of input") $
            \s -> case s of
                ':' -> Cons <$> readSExpr <*> readSExpr
                '.' -> pure Tip
@@ -22,8 +23,9 @@ toForest Tip  = []
 toTree (Leaf c) = Node [c] []
 toTree t = Node "*" (toForest t)
 
-sampleInput = ":a:b::c:d:e.:f:g:h." ++ error "oops"
-sampleResult = drawForest $ toForest $ hd $ evalR sampleInput $ fromP readSExpr Done
+sampleInput = ":a:b::c:d:e.:f:g:h.asdfs"
+sampleResult = -- drawForest $ toForest $ 
+               hd $ evalR sampleInput $ fromP ((,) <$> readSExpr <*> eof) Done
 
 
 
