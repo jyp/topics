12e34a018b5e1849ce3af61602c51c036c564825
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2008-11-07 11:02:10 +0100

    shuffle things around

--- Machines/SuspensionAM.hs
+++ Machines/SuspensionAM.hs
@@ -4,15 +4,28 @@
 import Control.Applicative
 
 
-data Term a where
-     Lam :: (Term a -> Term b) -> Term (a -> b)
-     App :: Term (a -> b) -> Term a -> Term b
-     Con :: a -> Term a
+data Ter a where
+     Lam :: (Ter a -> Ter b) -> Ter (a -> b)
+     App :: Ter (a -> b) -> Ter a -> Ter b
+     Con :: a -> Ter a
 
-     Disj :: Term (f a -> c) -> Term (g a -> c) -> Term ((f :+: g) a -> c)
-     Conj :: Term (f a -> g a -> c) -> Term ((f :*: g) a -> c)
-     Kons :: (x -> Term c) -> Term (K x a -> c)
-     Wrap :: Term (f (T f) -> c) -> Term ((T f) -> c)
+data Term a where
+     Lamb :: State t -> (Term a -> Term b) -> Term (a -> b)
+     Appl :: State t -> Term (a -> b) -> Term a -> Term b
+     Cons :: State t -> a -> Term a
+
+
+the current arg will be put in a lam; entered later.
+--> need to remember where to store the state upon entering the arg.
+--> this is the rhs of the Appl.
+
+cbnExec :: State t -> t
+cbnExec (State (App t1 t2) s)      = cbnExec (State t1 (Cons t2 s))
+cbnExec (State (Lam f) (Cons a s)) = cbnExec (State (f a) s)
+cbnExec (State (Lam f) Nil) = \x -> cbnExec (State (f (Con x)) Nil)
+-- this is a rule that cannot be in the untyped version.
+cbnExec (State (Con x) s) = app x s
+
 
 
 instance Functor Term where
@@ -30,52 +43,14 @@ eval :: Term t -> t
 eval (App t1 t2) = eval t1 (eval t2)
 eval (Lam f) = \x -> eval (f (Con x))
 eval (Con x) = x
-eval (Disj f g) = \x -> case x of
-    Inl a -> eval (f <*> pure a)
-    Inr a -> eval (g <*> pure a)
-eval (Conj f) = \(x :*: y) -> eval (f <*> pure x <*> pure y)
-eval (Wrap f) = \(In x) -> eval (f <*> pure x)
+
 
 
 instance Monad Term where
     return = Con
     k >>= f = f (eval k)
 
-data T (f :: * -> *) where
-    In :: f (T f) -> T f
-    Suspend :: T f
-out (In x) = x
 
-infixr :+:
-data (:+:) f g a where
-    Inl :: f a -> (:+:) f g a
-    Inr :: g a -> (:+:) f g a
-
-instance (Functor f, Functor g) => Functor (f :+: g) where
-    fmap h (Inl x) = Inl (fmap h x)
-    fmap h (Inr x) = Inr (fmap h x)
-
-infixr :*:
-data (:*:) f g a where
-    (:*:) :: f a -> g a -> (:*:) f g a
-
-instance (Functor f, Functor g) => Functor (f :*: g) where
-    fmap h (x :*: y) = fmap h x :*: fmap h y
-
-data K x a = K {fromK :: x}
-instance Functor (K x) where
-    fmap f (K x) = K x
-
-data Id a = Id {fromId :: a}
-instance Functor (Id) where
-    fmap f (Id a) = Id (f a)
-
-type List x = T (K () :+: (K x :*: Id))
-
-prod :: Term (List Int -> Int)
-prod = Wrap $ Disj
-       (Kons $ \_ -> pure 1)
-       (Conj $ Lam $ \x -> Lam $ \xs -> (*) <$> (fromK <$> x) <*> (prod <*> (fromId <$> xs)))
 
 
 
--- Machines/SuspensionPMatchAM.hs
+++ Machines/SuspensionPMatchAM.hs
@@ -4,12 +4,13 @@
 import Control.Applicative
 
 
-data Term input output where
-     App :: Term input (a -> b) -> Term input a -> Term input b
-     Con :: a -> Term input a
+data Term a where
+     Lam :: (Term a -> Term b) -> Term (a -> b)
+     App :: Term (a -> b) -> Term a -> Term b
+     Con :: a -> Term a
 
-     Disj :: Term f c -> Term g c -> Term (f :+: g) c
-     Conj :: Term (f a -> g a -> c) -> Term (f :*: g) c
+     Disj :: Term (f a -> c) -> Term (g a -> c) -> Term ((f :+: g) a -> c)
+     Conj :: Term (f a -> g a -> c) -> Term ((f :*: g) a -> c)
      Kons :: (x -> Term c) -> Term (K x a -> c)
      Wrap :: Term (f (T f) -> c) -> Term ((T f) -> c)
 
@@ -33,6 +34,7 @@ eval (Disj f g) = \x -> case x of
     Inl a -> eval (f <*> pure a)
     Inr a -> eval (g <*> pure a)
 eval (Conj f) = \(x :*: y) -> eval (f <*> pure x <*> pure y)
+eval (Wrap f) = \(In x) -> eval (f <*> pure x)
 
 
 instance Monad Term where
@@ -41,6 +43,7 @@ instance Monad Term where
 
 data T (f :: * -> *) where
     In :: f (T f) -> T f
+    Suspend :: T f
 out (In x) = x
 
 infixr :+:
@@ -77,7 +80,3 @@ prod = Wrap $ Disj
 
 
 
----------------------------
--- Data-driven evaluation
-
-deval 
