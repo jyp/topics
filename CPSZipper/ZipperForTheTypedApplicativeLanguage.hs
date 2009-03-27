4bb13197042799fa803081edc8e7ca220b8a559d
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2009-03-27 14:05:59 +0100

    Added ZipperForTheTypedApplicativeLanguage.hs

---
+++ CPSZipper/ZipperForTheTypedApplicativeLanguage.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE ImpredicativeTypes, ScopedTypeVariables, TypeFamilies, GADTs, EmptyDataDecls #-}
+
+import Prelude hiding (Left, Right)
+
+data Appl a where
+    (:*:) :: Appl (a -> b) -> Appl a -> Appl b
+    Pure  :: a -> Appl a
+
+
+data Context hole result where
+    Root :: Context hole hole
+    Left :: Context hole result -> Appl a -> Context (a -> hole) result
+    Right :: Appl (hole -> hole') -> Context hole' result -> Context hole result
+
+plug :: Context hole result -> Appl hole -> Appl result
+plug Root x = x
+plug (Left z r) x = plug z (x :*: r)
+plug (Right l z) x = plug z (l :*: x)
+
+
+data Zipper result where
+    Zip :: Context hole result -> Appl hole -> Zipper result
+
+up :: Zipper a -> Zipper a
+up (Zip (Left ctx b) a) = Zip ctx (a :*: b)
+up (Zip (Right a ctx) b) = Zip ctx (a :*: b)
+up (Zip Root _) = error "All the way up"
+
+downLeft :: Zipper result -> Zipper result
+downLeft  (Zip ctx (a :*: b)) = Zip (Left ctx b) a
+downLeft  (Zip ctx (Pure x)) = error "All the way down"
+
+downRight (Zip ctx (a :*: b)) = Zip (Right a ctx) b
+downRight (Zip ctx (Pure x)) = error "All the way down"
+
+
+
